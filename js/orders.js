8d68605e457507fe20e1686731387990be2c9fc2
ayrtonviola <153876697+ayrtonviola@users.noreply.github.com>
2025-08-22 21:15:20 -0300

    Update orders.js

--- js/orders.js
+++ js/orders.js
@@ -1,1 +1,6 @@
+// Aqui vamos implementar futuramente a listagem de pedidos do Firestore
+export const loadOrders = () => {
+  const container = document.getElementById("order-list");
+  container.innerHTML = `<p class="text-gray-600">Seus pedidos aparecerão aqui em breve.</p>`;
+};
 
